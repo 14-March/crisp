b323bfd11352bd0a447c500b7fec28295939aecb
Ycz-14March <372307094@qq.com>
2023-04-14 13:11:02 +0800

    go mod

--- server.go
+++ server.go
@@ -7,17 +7,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path"
+
+	"github.com/hcraM41/crisp/comm/clog"
 )
 
 func main() {
 	fmt.Println("hello crisp~")
 
-	//ex, err := os.Executable()
-	//
-	//if nil != err {
-	//	panic(err)
-	//}
-	//
-	////clog.Config(path.Dir(ex) + "/log/crisp.log")
-	////clog.Info("hihihihihi")
+	ex, err := os.Executable()
+
+	if nil != err {
+		panic(err)
+	}
+
+	clog.Config(path.Dir(ex) + "/log/crisp.log")
+	clog.Info("hihihihihi")
+
 }
